59982a5758807d0eaed946a8a1dfb9a4f21e3c32
iqbalzayn01 <miqzayudevstudio@gmail.com>
2023-11-22 11:20:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F5F5F5",
+      },
+    },
+  },
+  plugins: [],
+};
