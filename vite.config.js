cbe072a654aa4cdf2ef3b04dc3379fd1db81d348
iqbalzayn01 <miqzayudevstudio@gmail.com>
2023-12-21 11:04:22 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        home: "./src/pages/Home.jsx",
+        reservation: "./src/pages/Reservation.jsx",
+      },
+    },
+  },
+});
